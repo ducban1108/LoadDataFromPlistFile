cf3a62da4dae3f1db784ced5c4d105bbecf867eb
Bẩn <levietduc1108@gmail.com>
2019-04-17 10:50:15 +0700

    done

---
+++ LoadData_Fromplist/Controller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LoadData_Fromplist
+//
+//  Created by Just Kidding on 4/12/19.
+//  Copyright © 2019 Just Kidding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelShow: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
